290d3b32ecacde20ac752393ef8f913d48a93359
Ross Hutchens <rlhcxm@gmail.com>
2020-07-23 09:15:37 -0500

    set up rootSagas

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -8,13 +8,24 @@ const mapStateToProps = reduxState => ({
 class PlantList extends Component {
     componentDidMount() {
         // use component did mount to dispatch an action to request the plantList from the API
+        this.props.dispatch({type: "FETCH_PLANT"})
     }
 
     render() {
         return (
             <div>
                 <h3>This is the plant list</h3>
-                <pre>{JSON.stringify(this.props.reduxState)}</pre>
+                <pre>{JSON.stringify(this.props.reduxState.plantList)}</pre>
+                <ul>
+                    {this.props.reduxState.plantList.map((plant, index)=>{
+                        return <li key={index}>{plant.name}</li>
+                    })}
+                </ul>
+                {/* <ul>
+                    {this.props.reduxState.plantList.map((item, index)=>{
+                        return <li key={index}>{item.name}
+                        <button>Delete</button></li>})}
+                </ul>   */}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
+import {logger} from 'redux-logger';
 import App from './App';
+import createSagaMiddleware from 'redux-saga'
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
+function* rootSaga() {
+  yield takeEvery('FETCH_PLANT', getPlantSaga);
+  // yield takeEvery('ADD_FRUIT', addFruitSaga);
+}
 
-const plantList = (state = startingPlantArray, action) => {
+function* getPlantSaga(action){
+  try {
+    // yield console.log('attempting get');
+    const response = yield axios.get('/api/plant/');
+    yield console.log('back with:', response.data);
+    yield put( { type: 'ADD_PLANT', payload: response.data } );
+  } catch (error) {
+    console.log('error with element get request', error);
+  }
+}
+
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
       return [ ...state, action.payload ]
@@ -20,9 +38,12 @@ const plantList = (state = startingPlantArray, action) => {
       return state;
   }
 };
-
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger),
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
